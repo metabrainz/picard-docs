b59cf03475880dff40aca781bac94c5b35749f48
Bob Swift <bswift@rsds.ca>
2020-11-21 17:41:11 -0700

    Changes to support version and language selection

--- conf.py
+++ conf.py
@@ -122,7 +122,7 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-# html_js_files = ['/version_links.js']
+html_js_files = ['/version_links.js']
 
 html_context = {
     'extra_css_files': [
@@ -130,6 +130,7 @@ html_context = {
     ],
     'default_language': default_language,
     'supported_languages': supported_languages,
+    'release': release,
     'releases': release_list,
 }
 
@@ -149,8 +150,8 @@ latex_documents = [
 # latex_toplevel_sectioning = 'chapter'
 
 # latex_show_urls = 'inline'
-latex_show_urls = 'footnote'
-# latex_show_urls = 'no'
+# latex_show_urls = 'footnote'
+latex_show_urls = 'no'
 
 latex_elements = {
     'papersize': 'letterpaper',
