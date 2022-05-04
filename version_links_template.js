e0d06b75fbbc0bc1cedf9dfd46ffe2b2de8eaab5
Bob Swift <bswift@rsds.ca>
2022-05-04 17:24:54 -0600

    Modify template to support translation of 'stable' and 'latest' strings

--- version_links_template.js
+++ version_links_template.js
@@ -12,14 +12,14 @@ var vcount = versions.length;
 var i, text;
 var serv = window.location.protocol + '//' + window.location.hostname
 
-function list_versions(r_language, r_page) {
+function list_versions(r_language, r_page, r_stable_text = 'stable', r_latest_text = 'latest') {
     text = "\n";
     for (i = 0; i < vcount; i++) {
         // text += '<dd><a href="/' + versions[i] + '/index.html?language=' + r_language + '&page=' + r_page + '" class="__ptNoRemap">' + versions[i] + "</a></dd>\n";
         text += '<dd><a href="/v' + versions[i] + '/index.html?language=' + r_language + '&page=' + r_page + '" class="__ptNoRemap">' + versions[i] + "</a></dd>\n";
     }
-    text += '<dd><a href="/v' + stable_version + '/index.html?language=' + r_language + '&page=' + r_page + '" class="__ptNoRemap">stable' + "</a></dd>\n";
+    text += '<dd><a href="/v' + stable_version + '/index.html?language=' + r_language + '&page=' + r_page + '" class="__ptNoRemap">' + r_stable_text + "</a></dd>\n";
     // text += '<dd><a href="/index.html" class="__ptNoRemap">latest' + "</a></dd>\n";
-    text += '<dd><a href="/index.html?language=' + r_language + '&page=' + r_page + '" class="__ptNoRemap">latest' + "</a></dd>\n";
+    text += '<dd><a href="/index.html?language=' + r_language + '&page=' + r_page + '" class="__ptNoRemap">' + r_latest_text + "</a></dd>\n";
     document.getElementById("select_version").innerHTML += text;
 }
